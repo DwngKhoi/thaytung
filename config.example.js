8af4e451ccd49414645695fdc31b9fedd5d57268
DwngKhoi <dangkhoilevn@gmail.com>
2026-06-24 03:12:15 +0700

    Add Google Sheets backend

---
+++ config.example.js
@@ -0,0 +1,5 @@
+// Copy file nay thanh config.js va dien URL Apps Script sau khi deploy.
+// Khi co GAS_API_URL, frontend se goi Google Sheet private thay cho Node/Mongo API.
+window.GAS_API_URL = 'https://script.google.com/macros/s/DEPLOYMENT_ID/exec';
+window.STUDENT_KEY = 'doi-key-hoc-sinh';
+window.TEACHER_KEY = 'doi-key-giao-vien';
---
+++ config.js
@@ -0,0 +1,3 @@
+window.GAS_API_URL = 'https://script.google.com/macros/s/AKfycbyLqi-899NT6XzliDopicRcbgxHomo4jwjetRAUlF6KXGss8zSTBB9jdpQFFEs8xW0MRA/exec';
+window.STUDENT_KEY = 'hs-giangtung';
+window.TEACHER_KEY = 'gv-giangtung';
