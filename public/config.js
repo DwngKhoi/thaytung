0f919fd0401b934301809d0cb502d386cc04c886
DwngKhoi <dangkhoilevn@gmail.com>
2026-06-24 23:13:43 +0700

    Switch config to Supabase

--- public/config.js
+++ public/config.js
@@ -1,3 +1,5 @@
+window.SUPABASE_URL = 'https://rkyijsnsuulcmslakfhq.supabase.co';
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJreWlqc25zdXVsY21zbGFrZmhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzMTIwMTQsImV4cCI6MjA5Nzg4ODAxNH0.Dvkj0NiX77sTTnUR6TMwj49ZjQUveBNsNnbJyoiA7Es';
 window.GAS_API_URL = 'https://script.google.com/macros/s/AKfycbyLqi-899NT6XzliDopicRcbgxHomo4jwjetRAUlF6KXGss8zSTBB9jdpQFFEs8xW0MRA/exec';
 window.STUDENT_KEY = 'hs-giangtung';
 window.TEACHER_KEY = 'gv-giangtung';
